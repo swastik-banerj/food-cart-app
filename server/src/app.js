9ecb8f44370402c54b1f0ceeec5c70840763f81b
Swastik Banerjee <swastikb.aot@gmail.com>
2025-07-17 18:46:41 +0530

    Added frontend link in cors

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "food-cart-mld0gz7n2-swastik-banerjs-projects.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
