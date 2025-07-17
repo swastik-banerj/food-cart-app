b5b7c934d032410948eaf254fc059c4045a317c9
Swastik Banerjee <swastikb.aot@gmail.com>
2025-07-17 19:12:30 +0530

    fixed cors error

--- server/src/app.js
+++ server/src/app.js
@@ -6,12 +6,24 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+    "https://food-cart-app-delta.vercel.app",
+    "https://food-cart-9hjvmdxj0-swastik-banerjs-projects.vercel.app"
+];
+
 app.use(cors({
-    origin: "https://food-cart-app-delta.vercel.app",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
